b7ee17a4bc975eade86a6dfa2062dfa9e6e5b86c
DeVesco Pierre <drpierre.projects@gmail.com>
2020-08-27 02:44:26 +0000

    Execute docker image into Kubernete and created script to automate task

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=nabisco83/devops-training-machine-learning:completed
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl apply -f ./docker-compose.yml
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods -l app=udacity -o wide
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl get svc -l app=udacity -o wide
